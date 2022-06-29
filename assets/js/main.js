a3f22bc59557d12ad1f672e6e3400b3e6f7f35ef
elarium <69206434+just4ius@users.noreply.github.com>
2022-06-29 22:51:56 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 function submit() {
-	// const request = new XMLHttpRequest();
-	// request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
-	// request.setRequestHeader("Content-type", "application/json");
+	const request = new XMLHttpRequest();
+	request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
+	request.setRequestHeader("Content-type", "application/json");
 	var url_string = window.location.href;
 	var url = new URL(url_string);
-	const code = url.searchParams.get("code");
+	var code = url.searchParams.get("code");
 
 	console.log(code);
+	const params = {
+		content: "Webhook test"
+	}
+	request.send(JSON.stringify(params));
 }
