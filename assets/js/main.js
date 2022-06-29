ee1a4ba5b1f4a3ae4189f3b2737d8b4cd6ab332f
elarium <69206434+just4ius@users.noreply.github.com>
2022-06-29 22:46:22 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("123");
+function submit() {
+	// const request = new XMLHttpRequest();
+	// request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
+	// request.setRequestHeader("Content-type", "application/json");
+	var url_string = window.location.href;
+	var url = new URL(url_string);
+	const code = url.searchParams.get("code");
+
+	console.log(code);
+}
