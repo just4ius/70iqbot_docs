65d600179e7483df516bbce952b1a0ad2ced2fc7
elarium <69206434+just4ius@users.noreply.github.com>
2022-09-27 22:47:50 +0300

    Update m.js

--- assets/js/m.js
+++ assets/js/m.js
@@ -2,6 +2,6 @@ const colors = ["#0B3954", "#BFD7EA", "#FF6663", "#E0FF4F", "#FEFFFE"];
 var item1 = colors[Math.floor(Math.random() * colors.length)];
 var item2 = colors[Math.floor(Math.random() * colors.length)];
 
-var body = document.getElementById("gradient");
+var body = document.getElementById("page_header");
 
 body.style.background = "linear-gradient(90deg, "+item1+", "+item2+")";
