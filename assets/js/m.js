8ad13269671c398f6c84bb96c6c31c9b2025f538
elarium <69206434+just4ius@users.noreply.github.com>
2022-09-29 19:10:45 +0300

    Update m.js

--- assets/js/m.js
+++ assets/js/m.js
@@ -1,4 +1,4 @@
-const colors = [["#B7F0AD", "#D2FF96", "#EDFF7A", "#E8D33F", "#D17B0F"], ["#A3320B", "#D5E68D", "#6B0504", "#241023"], ["#F0F3BD", "#02C39A", "#00A896", "#028090", "#05668D"], ["#8E0045", "#9E0031", "#600047"], ["#D3C4E3", "#8F95D3", "#89DAFF"]];
+const colors = [["#B7F0AD", "#D2FF96", "#EDFF7A", "#E8D33F", "#D17B0F"], ["#A3320B", "#D5E68D", "#6B0504", "#241023"], ["#F0F3BD", "#02C39A", "#00A896", "#028090", "#05668D"], ["#8E0045", "#9E0031"], ["#D3C4E3", "#8F95D3", "#89DAFF"]];
 
 var random = Math.floor(Math.random() * colors.length)
 var color1 = colors[random][Math.floor(Math.random() * colors[random].length)];
