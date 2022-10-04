4b796c77970051f9b6f5678bc5839b2997ea28c5
elarium <69206434+just4ius@users.noreply.github.com>
2022-10-04 19:23:02 +0300

    Update m.js

--- assets/js/m.js
+++ assets/js/m.js
@@ -1,23 +1,32 @@
 const colors = [["B7F0AD", "D2FF96", "EDFF7A", "E8D33F", "D17B0F"], ["A3320B", "D5E68D", "6B0504", "241023"], ["F0F3BD", "02C39A", "00A896", "028090", "05668D"], ["D3C4E3", "8F95D3", "89DAFF"]];
 
 var url = new URL(window.location.href);
-if (url.searchParams.get("c1") !== none && url.searchParams.get("c2") !== none) {
+function translate_params() {
    var color1 = url.searchParams.get("c1");
    var color2 = url.searchParams.get("c2");
+
+   var body0 = document.getElementById("header0");
+   body0.style.background = "linear-gradient(90deg, #"+color1+", #"+color2+")";
+   
+   window.history.pushState({}, document.title, window.location.pathname);   
+}
+
+if (url.searchParams.get("c1") !== null && url.searchParams.get("c2") !== null) {
+   translate_params();
 } else {
-  var random = Math.floor(Math.random() * colors.length)
-  var color1 = colors[random][Math.floor(Math.random() * colors[random].length)];
-  var color2 = colors[random][Math.floor(Math.random() * colors[random].length)];
+   var random = Math.floor(Math.random() * colors.length)
+   var color1 = colors[random][Math.floor(Math.random() * colors[random].length)];
+   var color2 = colors[random][Math.floor(Math.random() * colors[random].length)];
 }
 var body = document.getElementById("header");
 var body0 = document.getElementById("header0");
 
 try {
-  body.style.background = "linear-gradient(90deg, #"+color1+", #"+color2+")";
+   body.style.background = "linear-gradient(90deg, #"+color1+", #"+color2+")";
 } catch (e) {
-  if (e instanceof TypeError) {
-    // pass
-  }
+   if (e instanceof TypeError) {
+      // pass
+   }
 }
 body0.style.background = "linear-gradient(90deg, #"+color1+", #"+color2+")";
 
@@ -26,5 +35,5 @@ body0.style.background = "linear-gradient(90deg, #"+color1+", #"+color2+")";
 // }, 2000);
 
 function translate_link() {
-  window.location.href = "/docs/en/?c1="+color1+"&c2="+color2;
+   window.location.href = "/docs/en/?c1="+color1+"&c2="+color2;
 }
